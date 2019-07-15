56e29cd77d47a1d76684811d5a1df3ed9c25204d
Perspective[S] <stephane@perspective-s.org>
2019-07-15 13:44:46 +0200

    Update canvas.js

--- demo/src/canvas.js
+++ demo/src/canvas.js
@@ -1,13 +1,13 @@
-import THREE from 'three';
-const OrbitControls = require('three-orbit-controls')(THREE);
+import { PerspectiveCamera, Scene, WebGLRenderer  } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export const renderer = () => {
-  const scene = new THREE.Scene();
-  const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1 / 10000);
+  const scene = new  Scene();
+  const cam = new  PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1 / 10000);
 
   const controls = new OrbitControls(cam);
 
-  const rend = new THREE.WebGLRenderer();
+  const rend = new  WebGLRenderer();
   rend.setSize(window.innerWidth, window.innerHeight);
   rend.setClearColor(0xff6347);
   cam.position.z = 1000;
