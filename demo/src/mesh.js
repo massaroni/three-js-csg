2a7f56cf04ea90c93354639a005b662b99a0d3fb
Perspective[S] <stephane@perspective-s.org>
2019-07-15 13:47:10 +0200

    Update mesh.js

--- demo/src/mesh.js
+++ demo/src/mesh.js
@@ -1,9 +1,9 @@
-import THREE from 'three';
-const ThreeBSP = require('../../index.js')(THREE);
+import { Mesh, BoxGeometry, SphereGeometry, MeshPhongMaterial } from 'three';
+import { ThreeBSP } from '../../index.js' 
 
 export const meshFactory = () => {
-  const box = new THREE.Mesh(new THREE.BoxGeometry(500, 100, 100));
-  const sphere = new THREE.Mesh(new THREE.SphereGeometry(100, 50, 50));
+  const box = new  Mesh(new BoxGeometry(500, 100, 100));
+  const sphere = new  Mesh(new  SphereGeometry(100, 50, 50));
 
   const sBSP = new ThreeBSP(sphere);
   const bBSP = new ThreeBSP(box);
@@ -11,7 +11,7 @@ export const meshFactory = () => {
   const sub = bBSP.subtract(sBSP);
   const newMesh = sub.toMesh();
 
-  newMesh.material = new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x1a1a1a, shininess: 30, shading: THREE.FlatShading });
+  newMesh.material = new  MeshPhongMaterial({ color: 0xdddddd, specular: 0x1a1a1a, shininess: 30, shading: THREE.FlatShading });
 
   return Object.assign({}, { csg: newMesh });
 };
